165ec74a17456a87cd2012653f03a7021e57d932
Munir_officePC <munir0aw0@gmail.com>
2025-12-24 13:56:02 +0300

    feat: remove unused files

--- doctype/r_and_d_kpi/r_and_d_kpi.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Your Company and contributors
-# For license information, please see license.txt
-
-import frappe
-from frappe.model.document import Document
-
-
-class RandDKpi(Document):
-	pass
